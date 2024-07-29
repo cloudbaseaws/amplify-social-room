ad8e03efbc241120c59c4027c6e9a9ecff38e627
Cloudbaseaws <161646401+cloudbaseaws@users.noreply.github.com>
2024-07-29 12:27:43 -0700

    Second Commit with file changes

--- src/RoomSelector.tsx
+++ src/RoomSelector.tsx
@@ -1,29 +1,47 @@
-import { Schema } from "../amplify/data/resource"
+//Add {type Schema} and next line add import {generateClient}
+
+import { type Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 import { defaultRoom } from "./utils";
 import { useEffect, useState } from "react";
-
+//Add this line below
+const client = generateClient<Schema>();
 export function RoomSelector({
   currentRoomId,
-  onRoomChange
+  onRoomChange,
 }: {
-  currentRoomId: string,
-  onRoomChange: (roomId: string) => void
+  currentRoomId: string;
+  onRoomChange: (roomId: string) => void;
 }) {
+  const [rooms, setRooms] = useState<Schema["Room"]["type"][]>([defaultRoom]);
 
-  const [rooms, setRooms] = useState<Schema["Room"]["type"][]>([defaultRoom])
-  
   useEffect(() => {
-    // Add observeQuery code here
-  }, [])
+    // Add observeQuery code here set up a live feed inside the useEffect
+    const sub = client.models.Room.observeQuery().subscribe({ next:(data)=>{setRooms([defaultRoom,...data.items])
 
-  return <>
-    <select
-      onChange={e => onRoomChange(e.target.value)}
-      value={currentRoomId}>
-      {rooms.map(room => <option value={room.id} key={room.id}>{room.topic}</option>)}
-    </select>
-    <button onClick={async () => {
-      // Add create Room logic here
-    }}>[+ add]</button>
-  </>
+    }
+  })
+  return (
+    ()=> sub.subscribe()
+  
+    <>
+      <select
+        onChange={(e) => onRoomChange(e.target.value)}
+        value={currentRoomId}
+      >
+        {rooms.map((room) => (
+          <option value={room.id} key={room.id}>
+            {room.topic}
+          </option>
+        ))}
+      </select>
+      <button
+        onClick={async () => {
+          // Add create Room logic here
+        }}
+      >
+        [+ add]
+      </button>
+    </>
+  );
 }
